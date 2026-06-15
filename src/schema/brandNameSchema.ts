6e25d10cf8dd794d96eba16ec0f1ae40e53d89b6
websidharth <websidharth@gmail.com>
2026-06-15 11:13:43 -0700

    fix

--- src/schema/brandNameSchema.ts
+++ src/schema/brandNameSchema.ts
@@ -1,11 +1,10 @@
-import * as Yup from 'yup';
 import { CreateBrandNameModel } from '@/models/brand-name.model';
+import * as Yup from 'yup';
 
 const BrandNameSchema: Yup.ObjectSchema<CreateBrandNameModel> = Yup.object().shape({
-    brandName: Yup.string().required('Brand name is required'),
-    status: Yup.string().required('Status is required'),
-    displayOrder: Yup.number().optional().default(0),
-    categoryIds: Yup.array().of(Yup.number().required()).optional().default([]),
+  name: Yup.string().required('Brand name is required'),
+  status: Yup.string().required('Status is required'),
+  displayOrder: Yup.number().optional().default(0),
 });
 
 export default BrandNameSchema;
